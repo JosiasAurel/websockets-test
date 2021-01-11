ec8fb279f4250943ed05d08ccab3021a5d112891
JosiasAurel <ndjosiasaurel@gmail.com>
2021-01-11 18:55:10 +0100

    create app

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('Hello World!');
+
+const form = document.getElementById('form')
+
+let msg = document.getElementById('msg')
+
+let resp = document.getElementById('res')
+
+let ws = new WebSocket("ws://localhost:8000")
+
+ws.onopen = alert("Connected")
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  
+  
+  let msg = document.getElementById('msg')
+
+  ws.send(msg.value)
+  
+  ws.onmessage = function(data) {
+    let resp = document.getElementById('res')
+    resp.innerText = data.data
+  }
+})
